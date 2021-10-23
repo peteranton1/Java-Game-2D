d4b972015260eaf57a42424269dd3a08b1b959cb
peteranton1 <peteranton1@yahoo.com>
2021-10-23 15:40:19 +0100

    Swing1 step12

--- Java2D/src/main/java/com/derby/swing1/FormPanel.java
+++ Java2D/src/main/java/com/derby/swing1/FormPanel.java
@@ -32,5 +32,54 @@ public class FormPanel extends JPanel {
         setBorder(BorderFactory.createCompoundBorder(
                 outerBorder, innerBorder
         ));
+
+        setLayout(new GridBagLayout());
+
+        GridBagConstraints gc = new GridBagConstraints();
+        Insets inset5px = new Insets(0, 0, 0, 5);
+        Insets inset0px = new Insets(0, 0, 0, 0);
+
+        ////////////// First Row //////////////
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        gc.gridx = 0;
+        gc.gridy = 0;
+        gc.fill = GridBagConstraints.NONE;
+        gc.insets = inset5px;
+        gc.anchor = GridBagConstraints.LINE_END;
+        add(nameLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 0;
+        gc.insets = inset0px;
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(nameField, gc);
+
+        ////////////// Second Row //////////////
+        gc.weightx = 1;
+        gc.weighty = 0.1;
+
+        gc.gridx = 0;
+        gc.gridy = 1;
+        gc.insets = inset5px;
+        gc.anchor = GridBagConstraints.LINE_END;
+        add(occupationLabel, gc);
+
+        gc.gridx = 1;
+        gc.gridy = 1;
+        gc.insets = inset0px;
+        gc.anchor = GridBagConstraints.LINE_START;
+        add(occupationField, gc);
+
+        ////////////// Third Row //////////////
+        gc.weightx = 1;
+        gc.weighty = 2.0;
+
+        gc.gridx = 1;
+        gc.gridy = 2;
+        gc.insets = inset0px;
+        gc.anchor = GridBagConstraints.FIRST_LINE_START;
+        add(okBtn, gc);
     }
 }
